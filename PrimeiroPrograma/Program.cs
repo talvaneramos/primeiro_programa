74db38c0d74d29e43e789cff447ef2ffd027c9ae
talvaneramos <talvanesilvaramos@gmail.com>
2023-05-10 10:55:29 -0300

    PrimeiroPrograma

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeiroPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.Write("Informe seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Bem vindo(a): " + nome);
+
+            Console.ReadKey();
+        }
+    }
+}
